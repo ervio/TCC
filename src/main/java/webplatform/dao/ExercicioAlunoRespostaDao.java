fc23312fd030487c8f9fa3ec4efa6d5b110e466a
Everton_Da_Costa <Everton_Da_Costa@W10CTBB542.poa.amer.dell.com>
2018-04-29 01:29:09 -0300

    Removendo classes não mais utilizadas

--- src/main/java/webplatform/dao/ExercicioAlunoRespostaDao.java
+++
@@ -1,8 +0,0 @@
-package webplatform.dao;
-
-import webplatform.model.entity.ExercicioAlunoResposta;
-
-public interface ExercicioAlunoRespostaDao {
-
-	public ExercicioAlunoResposta saveOrUpdate(ExercicioAlunoResposta entity);
-}
